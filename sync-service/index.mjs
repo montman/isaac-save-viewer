cf28d97c317ffc05f0f06215573105e5a28771b4
montman <amatipierluigi@gmail.com>
2024-09-13 12:52:29 +0200

    filepath changed

--- sync-service/index.mjs
+++ sync-service/index.mjs
@@ -1,31 +1,32 @@
-import { watch } from 'fs';
-import { readFile } from 'fs/promises';
-import { initializeApp, cert } from 'firebase-admin/app';
+import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore, Timestamp } from "firebase-admin/firestore";
-const saveGamePath = "example.txt"//"persistentgamedata1.dat";
+import { watch } from "fs";
+import { readFile } from "fs/promises";
+const saveGamePath =
+  "C:/Users/amati/Documents/My Games/Binding of Isaac Repentance/persistentgamedata1.dat";
 var onUploadSaveGame = false;
 initializeApp({
-    credential: cert('cert.json')
-})
+  credential: cert("cert.json"),
+});
 const uploadSaveGame = async () => {
-    const db = getFirestore();
+  const db = getFirestore();
 
-    let fileContent = await readFile(saveGamePath, 'base64');
-    const data = {
-        content: fileContent,
-        date: Timestamp.now()
-    }
-    let id = new Date().getTime().toString();
-    await db.collection('savegames').doc(id).set(data);
-}
+  let fileContent = await readFile(saveGamePath, "base64");
+  const data = {
+    content: fileContent,
+    date: Timestamp.now(),
+  };
+  let id = new Date().getTime().toString();
+  await db.collection("savegames").doc(id).set(data);
+};
 watch(saveGamePath, async (eventType, filename) => {
-    if (eventType === 'change') {
-        if (!onUploadSaveGame) {
-            onUploadSaveGame = true;
-            setTimeout(() => {
-                onUploadSaveGame = false;
-            }, 3000);
-            await uploadSaveGame();
-        }
+  if (eventType === "change") {
+    if (!onUploadSaveGame) {
+      onUploadSaveGame = true;
+      setTimeout(() => {
+        onUploadSaveGame = false;
+      }, 3000);
+      await uploadSaveGame();
     }
-}); 
+  }
+});
