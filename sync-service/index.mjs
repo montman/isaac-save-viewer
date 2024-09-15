46a9cbcfb41635541d240d16c10eea6f443e9fa0
montman <amatipierluigi@gmail.com>
2024-09-15 15:19:55 +0200

    fixed

--- sync-service/index.mjs
+++ sync-service/index.mjs
@@ -1,32 +1,47 @@
+import { exec } from "child_process";
 import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore, Timestamp } from "firebase-admin/firestore";
-import { watch } from "fs";
 import { readFile } from "fs/promises";
+let isOpened = false;
 const saveGamePath =
   "C:/Users/amati/Documents/My Games/Binding of Isaac Repentance/persistentgamedata1.dat";
-var onUploadSaveGame = false;
 initializeApp({
   credential: cert("cert.json"),
 });
-const uploadSaveGame = async () => {
-  const db = getFirestore();
 
-  let fileContent = await readFile(saveGamePath, "base64");
-  const data = {
-    content: fileContent,
-    date: Timestamp.now(),
-  };
-  let id = new Date().getTime().toString();
-  await db.collection("savegames").doc(id).set(data);
+const isRunning = async (query) => {
+  return await new Promise((res, rej) => {
+    exec("tasklist", (err, stdout, stderr) => {
+      res(stdout.toLowerCase().indexOf(query.toLowerCase()) > -1);
+    });
+  });
 };
-watch(saveGamePath, async (eventType, filename) => {
-  if (eventType === "change") {
-    if (!onUploadSaveGame) {
-      onUploadSaveGame = true;
-      setTimeout(() => {
-        onUploadSaveGame = false;
-      }, 3000);
-      await uploadSaveGame();
-    }
+
+const uploadSaveGame = async () => {
+  try {
+    const db = getFirestore();
+
+    let fileContent = await readFile(saveGamePath, "base64");
+    const data = {
+      content: fileContent,
+      date: Timestamp.now(),
+    };
+    let id = new Date().getTime().toString();
+    await db.collection("savegames").doc(id).set(data);
+    console.log("uploaded save file");
+  } catch (err) {
+    console.log(err);
   }
-});
+};
+setInterval(async () => {
+  let isCurrentlyRunning = await isRunning("isaac");
+  if (isOpened && !isCurrentlyRunning) {
+    console.log("isaac is closed");
+    isOpened = false;
+    await uploadSaveGame();
+  }
+  if (!isOpened && isCurrentlyRunning) {
+    console.log("isaac is opened");
+    isOpened = true;
+  }
+}, 5000);
