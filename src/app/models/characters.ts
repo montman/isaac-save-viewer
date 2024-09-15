94ff92ed45dd6a6663533889d1a401a252ae9385
montman <amatipierluigi@gmail.com>
2024-09-15 18:01:18 +0200

    characters finished

---
+++ src/app/models/characters.ts
@@ -0,0 +1,244 @@
+export const CHARACTERS: {
+  name: string;
+  image: string;
+  achievementId?: number;
+  description: string;
+}[] = [
+  {
+    name: 'Isaac',
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/e/e5/Character_Isaac_appearance.png',
+    description: '',
+  },
+  {
+    name: 'Magdalene',
+    achievementId: 1,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/b/bf/Character_Magdalene_appearance.png/revision/latest?cb=20210824111834',
+    description: '',
+  },
+  {
+    name: 'Cain',
+    achievementId: 2,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/d/db/Character_Cain_appearance.png/revision/latest?cb=20211204211028',
+    description: '',
+  },
+  {
+    name: 'Judas',
+    achievementId: 3,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/8/8d/Character_Judas_appearance.png/revision/latest?cb=20210826071249',
+    description: '',
+  },
+  {
+    name: '???',
+    achievementId: 32,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/9/9a/Character_%3F%3F%3F_appearance.png/revision/latest?cb=20210824125207',
+    description: '',
+  },
+  {
+    name: 'Eve',
+    achievementId: 42,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/9/91/Character_Eve_appearance.png/revision/latest?cb=20210826071757',
+    description: '',
+  },
+  {
+    name: 'Samson',
+    achievementId: 67,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/8/8c/Character_Samson_appearance.png/revision/latest?cb=20210824125143',
+    description: '',
+  },
+  {
+    name: 'Azazel',
+    achievementId: 79,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/0/06/Character_Azazel_appearance.png/revision/latest?cb=20210824125228',
+    description: '',
+  },
+  {
+    name: 'Lazarus',
+    achievementId: 80,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/7/78/Character_Lazarus_appearance.png/revision/latest?cb=20211111110428',
+    description: '',
+  },
+  {
+    name: 'Eden',
+    achievementId: 81,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/8/83/Character_Eden_appearance.png/revision/latest?cb=20210826072021',
+    description: '',
+  },
+  {
+    name: 'The Lost',
+    achievementId: 82,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/f/f0/Character_The_Lost_appearance.png/revision/latest?cb=20210826071932',
+    description: '',
+  },
+  {
+    name: 'Lilith',
+    achievementId: 199,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/a/a8/Character_Lilith_appearance.png/revision/latest?cb=20210821134536',
+    description: '',
+  },
+  {
+    name: 'Keeper',
+    achievementId: 251,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/2/20/Character_Keeper_appearance.png/revision/latest?cb=20210821185529',
+    description: '',
+  },
+  {
+    name: 'Apollyon',
+    achievementId: 340,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/4/40/Character_Apollyon_appearance.png/revision/latest?cb=20210822021832',
+    description: '',
+  },
+  {
+    name: 'The Forgotten',
+    achievementId: 390,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/2/21/Character_The_Forgotten_appearance.png/revision/latest?cb=20210822091711',
+    description: '',
+  },
+  {
+    name: 'Bethany',
+    achievementId: 404,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/8/82/Character_Bethany_appearance.png/revision/latest?cb=20210822150024',
+    description: '',
+  },
+  {
+    name: 'Jacob and Esau',
+    achievementId: 405,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/1/1d/Character_Jacob_and_Esau_appearance.png/revision/latest?cb=20210824111920',
+    description: '',
+  },
+  {
+    name: 'Tainted Isaac',
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/e/ec/Character_Tainted_Isaac_appearance.png/revision/latest?cb=20210824112002',
+    achievementId: 474,
+    description: '',
+  },
+  {
+    name: 'Tainted Magdalene',
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/f/f1/Character_Tainted_Magdalene_appearance.png/revision/latest?cb=20210824112024',
+    achievementId: 475,
+    description: '',
+  },
+  {
+    name: 'Tainted Cain',
+    achievementId: 476,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/2/2e/Tainted_Cain_App.png/revision/latest?cb=20210825173304',
+    description: '',
+  },
+  {
+    name: 'Tainted Judas',
+    achievementId: 477,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/0/0e/Tainted_Judas_App.png/revision/latest?cb=20210825173324',
+    description: '',
+  },
+  {
+    name: 'Tainted ???',
+    achievementId: 478,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/c/c1/Tainted_Blue_Baby_App.png/revision/latest?cb=20210825173344',
+    description: '',
+  },
+  {
+    name: 'Tainted Eve',
+    achievementId: 479,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/0/04/Tainted_Eve_App.png/revision/latest?cb=20210825173404',
+    description: '',
+  },
+  {
+    name: 'Tainted Samson',
+    achievementId: 480,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/7/72/Tainted_Samson_App.png/revision/latest?cb=20210825173424',
+    description: '',
+  },
+  {
+    name: 'Tainted Azazel',
+    achievementId: 481,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/6/61/Tainted_Azazel_App.png/revision/latest?cb=20210825173445',
+    description: '',
+  },
+  {
+    name: 'Tainted Lazarus',
+    achievementId: 482,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/7/70/Character_Tainted_Lazarus_appearance.png/revision/latest?cb=20230111000758',
+    description: '',
+  },
+  {
+    name: 'Tainted Eden',
+    achievementId: 483,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/5/57/Character_Tainted_Eden_appearance.png/revision/latest?cb=20210824112436',
+    description: '',
+  },
+  {
+    name: 'Tainted Lost',
+    achievementId: 484,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/b/b7/Tainted_Lost_App.png/revision/latest?cb=20210825173525',
+    description: '',
+  },
+  {
+    name: 'Tainted Lilith',
+    achievementId: 485,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/a/a7/Tainted_Lilith_App.png/revision/latest?cb=20210825173545',
+    description: '',
+  },
+  {
+    name: 'Tainted Keeper',
+    achievementId: 486,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/0/05/Tainted_Keeper_App.png/revision/latest?cb=20210825173606',
+    description: '',
+  },
+  {
+    name: 'Tainted Apollyon',
+    achievementId: 487,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/2/27/Tainted_Apollyon_App.png/revision/latest?cb=20210825173626',
+    description: '',
+  },
+  {
+    name: 'Tainted Forgotten',
+    achievementId: 488,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/1/1f/Character_Tainted_Forgotten_appearance.png/revision/latest?cb=20210824112044',
+    description: '',
+  },
+  {
+    name: 'Tainted Bethany',
+    achievementId: 489,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/e/ed/Tainted_Bethany_App.png/revision/latest?cb=20210825173646',
+    description: '',
+  },
+  {
+    name: 'Tainted Jacob',
+    achievementId: 490,
+    image:
+      'https://static.wikia.nocookie.net/bindingofisaacre_gamepedia/images/0/0b/Tainted_Jacob_App.png/revision/latest?cb=20210825173706',
+    description: '',
+  },
+];
